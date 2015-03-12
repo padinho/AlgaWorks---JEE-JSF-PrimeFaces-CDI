d84dd746ce7d84227e6e51be51f5f392b89f9565
Daniel-Dos <Daniel Dias@Desenvolvimento>
2015-03-12 16:46:05 -0300

    Projeto

---
+++ src/main/java/CriarTabelas.java
@@ -0,0 +1,11 @@
+
+import javax.persistence.Persistence;
+public class CriarTabelas {
+
+	public static void main(String[] args) {
+
+		Persistence.createEntityManagerFactory("FinanceiroPU");
+
+	}
+
+}
